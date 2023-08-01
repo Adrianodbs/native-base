45ca03d23be3f3f60abcb28fb6cd13c8c5e994b5
Adriano <adriano_alves_2008@hotmail.com>
2023-08-01 09:40:37 -0300

    Finalizando o header

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Box, HStack, Pressable, Icon } from 'native-base'
+import { Box, HStack, Pressable, Icon, Avatar } from 'native-base'
 import { Feather } from '@expo/vector-icons'
 
 const Home = () => {
@@ -15,6 +15,20 @@ const Home = () => {
         <Box>
           <Pressable>
             <Icon as={Feather} name="menu" size={7} color="#000" />
+          </Pressable>
+        </Box>
+
+        <Box flexDir="row" alignItems="center" rounded="md">
+          <Pressable marginRight={4}>
+            <Icon as={Feather} name="bell" size={7} color="#000" />
+          </Pressable>
+
+          <Pressable>
+            <Avatar
+              source={{ uri: 'https://sujeitoprogramador.com/steve.png' }}
+              height={12}
+              width={12}
+            />
           </Pressable>
         </Box>
       </HStack>
